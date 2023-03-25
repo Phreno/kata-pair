f71b300455ae65bdee3ca7b66f0b27606c4ac330
Phreno <phreno@pop-os.localdomain>
2023-03-25 23:19:08 +0100

    test(fizzbuzz): implemente le test buzz

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,4 +1,14 @@
 def fizzbuzz(arg: int):
-    if (arg % 3 == 0):
+    if (is_fizz(arg)):
         return "fizz"
+    if (is_buzz(arg)):
+        return "buzz"
     return arg
+
+
+def is_fizz(arg: int):
+    return arg % 3 == 0
+
+
+def is_buzz(arg: int):
+    return arg % 5 == 0
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -2,6 +2,7 @@ import unittest
 
 from fizzbuzz import fizzbuzz
 
+
 class TestFizzBuzz(unittest.TestCase):
     def test_1_doit_retourner_1(self):
         self.assertEqual(fizzbuzz(1), 1)
