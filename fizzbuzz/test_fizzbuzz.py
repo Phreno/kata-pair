22124a1db70430bc26decd0c137cea85b9f449f8
David Lejeune <david.a.lejeune@gmail.com>
2023-03-25 17:58:05 -0400

    test(fizzbuzz): ajoute le test fizz

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -5,3 +5,7 @@ from fizzbuzz import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
     def test_1_doit_retourner_1(self):
         self.assertEqual(fizzbuzz(1), 1)
+
+    def test_3_doit_retourner_fizz(self):
+        self.assertEqual(fizzbuzz(3), "fizz")
+
